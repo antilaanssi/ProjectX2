79a36987cee610537a475d4920397f65f488b2a4
antilaanssi <55877332+antilaanssi@users.noreply.github.com>
2023-09-12 14:36:44 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -11,10 +11,11 @@ FUZZ_TEST_SETUP() {
 }
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
-      FuzzedDataProvider fuzzed_data(data, size);
-      float first_fuzz = fuzzed_data.ConsumeFloatingPoint<float>();
-      float second_fuzz = fuzzed_data.ConsumeFloatingPoint<float>();
-      std::string random_string = fuzzed_data.ConsumeRandomLengthString();
+        FuzzedDataProvider fuzzed_data(data, size);
+        float a = fuzzed_data.ConsumeFloatingPoint<float>();
+        float b = fuzzed_data.ConsumeFloatingPoint<float>();
+        std::string c = fuzzed_data.ConsumeRandomLengthString();
+        calculator(a, c[0], b);
 
-      calculator(first_fuzz, random_string[0], second_fuzz);
+
 }
